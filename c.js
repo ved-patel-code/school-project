822b37e8b3499a7b28e6102ebb32bbab5ad55f6d
ved-patel-code <74485495+ved-patel-code@users.noreply.github.com>
2023-10-13 17:31:23 +0530

    Update c.js

--- c.js
+++ c.js
@@ -47,7 +47,7 @@ function submit() {
       a +
       "<br>" +
       "solar panel should face in south direction during installing "+"<br>"+
-      "total co2 emissions saved by solar plant = "+(x*30)*0.99+" KG" ;
+      "monthly co2 emissions saved by solar plant = "+(x*30)*0.99+" KG" ;
   } else if (south.checked) {
     document.getElementById("out").innerHTML =
       "total units in 1 day produce by plant = " +
@@ -60,7 +60,7 @@ function submit() {
       a +
       "<br>" +
       "solar panel should face in north direction during installing"+"<br>"+
-      "total co2 emissions saved by solar plant = "+(x*30)*0.99+" KG" ;
+      "monthly co2 emissions saved by solar plant = "+(x*30)*0.99+" KG" ;
   } else {
     document.getElementById("out").innerHTML = "please select location";
   }
